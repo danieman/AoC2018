29b07a3e381643c7ddd1e68b51de44a2402bffc3
danieman <34152459+danieman@users.noreply.github.com>
2018-12-03 00:05:16 +0100

    Clean commits

---
+++ 1/chronal.py
@@ -0,0 +1,27 @@
+file = [line.strip() for line in open('input.txt')]
+frequencies = set()
+freq = 0
+
+def looplist(loop=True):
+	global file, freq, frequencies
+	while True:
+		for line in file:
+			if line[0] == '+':
+				freq += int(line[1:])
+			elif line[0] == '-':
+				freq -= int(line[1:])
+
+			if freq in frequencies:
+				return freq
+			else:
+				frequencies.add(freq)
+		if loop == False:
+			return freq
+
+# a)
+print(f'a): {looplist(False)}')
+# Solution: 477
+
+# b)
+print(f'b): {looplist()}')
+# Solution: 390 (137 rounds)
